2592d8e2255bbacedb5cabefd8dac90a34f15b05
Álvaro Domínguez López <alvarodl20@outlook.com>
2020-11-21 21:24:24 +0100

    feat(global): initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+    printWidth: 100,
+    svelteSortOrder: 'styles-scripts-markup',
+    svelteStrictMode: false,
+    svelteBracketNewLine: true,
+  };
